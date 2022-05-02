321a86eb08228f067170f87bd97e718e318c4883
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2022-05-02 16:40:49 -0400

    added parragraphs to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -47,6 +47,52 @@ const about = () => {
           </p>
         </div>
         <CarouselComponent />
+        <div className="whyKw-parr">
+          <p>
+          <span>&#8212;</span> Why choose <span className="kwSpan">Keller Williams</span> <span>&#8212;</span>
+          </p>
+          <p className="subtitle">Technology</p>
+          <p>
+            Leading-edge tech tools and training give me the edge in effectively
+            marketing your property online, 24 hours a day, seven days a week!
+            Through KW’s exclusive Keller Williams Listing System (KWLS), your
+            property is fed to more than 350 online search engines and available
+            on KW’s Web network of more than 76,000 sites. Best of all, because
+            of Keller Williams Realty’s “My Listings, My Leads” philosophy,
+            every single Internet inquiry on your property will come directly to
+            me so that I can follow up quickly on potential buyers for your
+            property.
+          </p>
+          <p className="subtitle">Teamwork</p>
+          <p>
+            Keller Williams Realty was designed to reward agents for working
+            together. Based on the belief that we are all more successful if we
+            strive toward a common goal rather than our individual interests,
+            I’m confident that every Keller Williams professional shares the
+            common goal of serving you, my client, in the best way possible.
+          </p>
+          <p className="subtitle">Knowledge</p>
+          <p>
+            Keller Williams Realty helps me stay ahead of trends in the real
+            estate industry through its comprehensive, industry-leading training
+            curriculum and research resources. It’s what prepares me to provide
+            you with unparalleled service.
+          </p>
+          <p className="subtitle">Reliability</p>
+          <p>
+            Founded on the principles of trust and honesty, Keller Williams
+            Realty emphasizes the importance of having the integrity to do the
+            right thing, always putting your needs first. It reinforces my
+            belief that my success is ultimately determined by the legacy I
+            leave with each client I serve.
+          </p>
+          <p className="subtitle">Track Record</p>
+          <p>
+            I’m proud to work for #1 Real Estate Franchise in the World, by
+            agent count! It’s proof that when you offer a superior level of
+            service, the word spreads fast.
+          </p>
+        </div>
       </div>
     </>
   );
