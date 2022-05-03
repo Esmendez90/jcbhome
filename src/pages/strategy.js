d196a1915798d773eec6def0445bde45e675e6ba
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2022-05-03 12:44:21 -0400

    added strategy page, added image and styling

--- src/pages/strategy.js
+++ src/pages/strategy.js
@@ -1,9 +1,29 @@
-import React from 'react';
-const strategy = () =>{
+import React from "react";
+const strategy = () => {
   return (
-  <div>
-  <h1>Strategy</h1>
-  </div>
+    <>
+      <div className="topRedBlock"></div>
+      <div className="strategy-container">
+        <div className="planSuccess">
+          <p>WE BUILD THE PLAN</p>
+          <p>YOU BUILD SUCCESS</p>
+        </div>
+        <p>
+          There’s a lot that goes into selling your house, and it takes time,
+          effort, and expertise to truly do it right. Before you decide to sell
+          your house yourself, let's discuss your options to make sure you get
+          the most out of your sale.
+        </p>
+        <img src="/images/strategyimg.jpeg" alt="strategy path image" className="strategyPath-img"/>
+        <p>
+        Let's Chat!<br />
+        I'm sure you have questions and thoughts about the real estate process.
+        I'd love to talk with you about what you've read here and help you on
+        the path to selling your house. My contact information is on top, and I
+        look forward to working with you.
+      </p>
+      </div>
+    </>
   );
-}
+};
 export default strategy;
