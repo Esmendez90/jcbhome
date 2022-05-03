97a70b91ffd57efad45683cd6cfa439692792486
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2022-05-03 11:19:00 -0400

    added parragraphs to services pages, added styling

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,9 +1,70 @@
-import React from 'react';
-const services = () =>{
+import React from "react";
+const services = () => {
   return (
-  <div>
-  <h1>Services</h1>
-  </div>
+    <>
+      <div className="topRedBlock"></div>
+      <div className="services-container">
+        <div className="marketingStrat-container">
+          <p className="servicesTitles">MARKETING STRATEGIES</p>
+          <ul>
+            <li>Staging Consultation</li>
+            <li>Professional Photography</li>
+            <li>Yard sign and coming soon rider</li>
+            <li>Marketing Brochures and Just Listed Cards</li>
+            <li>
+              Property goes live and syndicated online 24/7 through more than
+              350 of the most popular searches Websites
+            </li>
+            <li>Social Media Marketing Just Listed cards mailed</li>
+            <li>Host Brokers Tour</li>
+            <li>Public Open House Weekly Update</li>
+          </ul>
+        </div>
+        <div className="marketingStrat-container">
+          <p className="servicesTitles">YOUR CUSTOM MARKETING PLAN</p>
+          <p className="servicesSecondTitle">SMART, TARGETED NETWORKING</p>
+          <p className="services-parr">
+            From dynamic fliers, to lead- optimized landing pages, to beautiful
+            postcards, to custom lookbooks, we'll determine what professionally
+            printed or digital marketing pieces will move the needle to maximize
+            the sale of your property. As an active member in the real estate
+            community and our community at large, I will market your listing to
+            top agents and buyer specialists in the area, generating excitement
+            and ensuring maximum exposure.
+          </p>
+        </div>
+
+        <div className="marketingStrat-container">
+          <p className="servicesTitles">PROMOTIONAL ASSETS... BEST IN CLASS!</p>
+          <ul>
+            <li>Our web network boasts 2.6 million unique visitors a month.</li>
+            <li>
+              Among the features specifically designed to give costumers all the
+              real estate information they want, are local featured listings on
+              the landing page and a mobie optimized version of the site.
+            </li>
+            <li>
+              Keller Williams associates enjoy the benefits of receiving text
+              messages directly from consumers through their listings.
+            </li>
+            <li>
+              Listings have increased open house visibility via a real-time open
+              house search.
+            </li>
+          </ul>
+        </div>
+        <div className="marketingStrat-container">
+          <p className="servicesTitles">INTERNET MARKETING</p>
+          <p className="servicesSecondTitle">EXTENDED MARKET REACH</p>
+          <p className="services-parr">
+            When you list with me, we'll have access to the Keller Williams
+            Listing System, or KWLS. This proprietary, exclusive system ensures
+            your property is marketed online 24/7 through more than 350 of the
+            most popular search Websites.
+          </p>
+        </div>
+      </div>
+    </>
   );
-}
+};
 export default services;
