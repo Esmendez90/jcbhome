0a1a4ab0e2c45cf3f3e5b0eff8dd08580850fc30
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2022-06-25 10:34:50 -0400

    added background cover image to WHYKW page, edited style, position, fonts

--- src/pages/whykw.js
+++ src/pages/whykw.js
@@ -1,67 +1,99 @@
 import React from "react";
+import "./style.css";
 const whyKW = () => {
-    return(
-        <>
-         <div className="whyKw-parr">
-          <div className="kwImages-container">
-            <img
-              src="/images/kwtechnology.png"
-              alt="keller william is #1 - spanish"
-            />
-            <img
-              src="/images/kwopportunity.png"
-              alt="keller william is #1 - spanish"
-            />
-            <img src="/images/kwknowledge.png" alt="keller williams cares" />
-            <img src="/images/kwsketch.png" alt="love keller william" />
+  return (
+    <>
+      <div className="why-kw-main-container">
+        <section id="page-header" className="background-cover-image">
+          <div className="text-container">
+            <h1>
+              WHY I CHOSE <br/> <span>KELLER WILLIAMS</span>
+            </h1>
           </div>
-          <p>
-            <span>&#8212;</span> Why I chose{" "}
-            <span className="kwSpan">Keller Williams</span> <span>&#8212;</span>
-          </p>
-          <p className="subtitle">Technology</p>
-          <p className="group1">
-            Leading-edge tech tools and training give me the edge in effectively
-            marketing your property online, 24 hours a day, seven days a week!
-            Through KW’s exclusive Keller Williams Listing System (KWLS), your
-            property is fed to more than 350 online search engines and available
-            on KW’s Web network of more than 76,000 sites. Best of all, because
-            of Keller Williams Realty’s “My Listings, My Leads” philosophy,
-            every single Internet inquiry on your property will come directly to
-            me so that I can follow up quickly on potential buyers for your
-            property.
-          </p>
-          <p className="subtitle group2">Teamwork</p>
-          <p className="group2">
-            Keller Williams Realty was designed to reward agents for working
-            together. Based on the belief that we are all more successful if we
-            strive toward a common goal rather than our individual interests,
-            I’m confident that every Keller Williams professional shares the
-            common goal of serving you, my client, in the best way possible.
-          </p>
-          <p className="subtitle">Knowledge</p>
-          <p className="group1">
-            Keller Williams Realty helps me stay ahead of trends in the real
-            estate industry through its comprehensive, industry-leading training
-            curriculum and research resources. It’s what prepares me to provide
-            you with unparalleled service.
-          </p>
-          <p className="subtitle group2">Reliability</p>
-          <p className="group2">
-            Founded on the principles of trust and honesty, Keller Williams
-            Realty emphasizes the importance of having the integrity to do the
-            right thing, always putting your needs first. It reinforces my
-            belief that my success is ultimately determined by the legacy I
-            leave with each client I serve.
-          </p>
-          <p className="subtitle">Track Record</p>
-          <p className="group1">
-            I’m proud to work for #1 Real Estate Franchise in the World, by
-            agent count! It’s proof that when you offer a superior level of
-            service, the word spreads fast.
-          </p>
-        </div>
-        </>
-    )
+        </section>
+
+        <section>
+          <div>
+            <img />
+          </div>
+          <div>
+            <h2>Technology</h2>
+            <p>
+              Leading-edge tech tools and training give me the edge in
+              effectively marketing your property online, 24 hours a day, seven
+              days a week! Through KW’s exclusive Keller Williams Listing System
+              (KWLS), your property is fed to more than 350 online search
+              engines and available on KW’s Web network of more than 76,000
+              sites. Best of all, because of Keller Williams Realty’s “My
+              Listings, My Leads” philosophy, every single Internet inquiry on
+              your property will come directly to me so that I can follow up
+              quickly on potential buyers for your property.
+            </p>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <img />
+          </div>
+          <h2>Teamwork</h2>
+          <div>
+            <p>
+              Keller Williams Realty was designed to reward agents for working
+              together. Based on the belief that we are all more successful if
+              we strive toward a common goal rather than our individual
+              interests, I’m confident that every Keller Williams professional
+              shares the common goal of serving you, my client, in the best way
+              possible.
+            </p>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <img />
+          </div>
+          <div>
+            <h2>Knowledge</h2>
+            <p>
+              Keller Williams Realty helps me stay ahead of trends in the real
+              estate industry through its comprehensive, industry-leading
+              training curriculum and research resources. It’s what prepares me
+              to provide you with unparalleled service.
+            </p>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <img />
+          </div>
+          <div>
+            <h2>Reliability</h2>
+            <p>
+              Founded on the principles of trust and honesty, Keller Williams
+              Realty emphasizes the importance of having the integrity to do the
+              right thing, always putting your needs first. It reinforces my
+              belief that my success is ultimately determined by the legacy I
+              leave with each client I serve.
+            </p>
+          </div>
+        </section>
+        <section>
+          <div>
+            <img />
+          </div>
+          <div>
+            <h2>Track Record</h2>
+            <p>
+              I’m proud to work for #1 Real Estate Franchise in the World, by
+              agent count! It’s proof that when you offer a superior level of
+              service, the word spreads fast.
+            </p>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 };
 export default whyKW;
