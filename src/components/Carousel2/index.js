0aa927383c047039ada6be2a910e68954af2d769
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2022-06-30 17:03:03 -0400

    added text over carousel images

--- src/components/Carousel2/index.js
+++ src/components/Carousel2/index.js
@@ -5,15 +5,22 @@ import images from "./images.json";
 export function CarouselComponent2() {
   return (
     <Carousel fade>
-      {images.map(({ id, className, src, alt }) => (
+      {images.map(({ id, className, src, alt, header1, text, text2 }) => (
         <Carousel.Item key={id}>
           <img className={className} src={src} alt={alt} />
-          
+          <section>
+            <div className="text-header-container services-text-header">
+              <h1>
+                {header1}
+              </h1>
+              <div className="services-text-container">
+                <p>{text}</p>
+                <p>{text2}</p>
+              </div>
+            </div>
+          </section>
         </Carousel.Item>
       ))}
     </Carousel>
-
-    
   );
-}
-// export default CarouselComponent2;
+};
