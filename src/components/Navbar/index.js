8627aa5cc3540c289da0c3318dbc19b1ef3b1ec4
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2024-08-07 10:06:28 -0400

    edited images path

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -16,7 +16,7 @@ function Navigationbar() {
             <ImgComponent
               imgContainerClass="logoImg-container"
               imgClass="logoImg"
-              src="/Images/logo.png"
+              src="/jcbhomes/Images/logo.png"
               alt="jcb logo"
             />
           </Navbar.Brand>
